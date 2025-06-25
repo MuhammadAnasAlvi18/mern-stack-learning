995d3b4ebd195e809e0affbc46eb4fa0bbd89f93
Muhammad Anas <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2025-06-25 20:20:26 +0500

    authentication complete

--- frontend/lib/axiosInstance.js
+++ frontend/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/auth" || "https://mern-stack-learning-backend.vercel.app/api/auth",
+    baseURL: "https://mern-stack-learning-backend.vercel.app/api/auth",
     withCredentials: true
 });
