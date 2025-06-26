b5ab960edfc504840fb41385fa64becd089b934c
Muhammad Anas <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2025-06-26 17:55:20 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,16 @@ app.use(cookieParser());
 // Routes
 app.use("/api/auth" , authRoutes)
 
+// Always connect to DB
+connectDB();
+
 // For local development
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server started on port ${PORT}`);
-        connectDB();
     });
 }
+
+// Export the app for Vercel serverless
+export default app;
