523a308b38727cce63843dc6248bc2323691470c
Muhammad Anas <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2025-06-26 17:59:12 +0500

    changes

--- frontend/lib/axiosInstance.js
+++ frontend/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: process.env.BASE_URL,
     withCredentials: true
 });
