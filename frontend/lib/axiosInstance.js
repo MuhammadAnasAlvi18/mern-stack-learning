8e1c89b5c74697a661a7a1df85e0f5d4d7ff4c1b
Muhammad Anas <81664171+MuhammadAnasAlvi18@users.noreply.github.com>
2025-06-26 18:04:26 +0500

    changes

--- frontend/lib/axiosInstance.js
+++ frontend/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: "https://mern-stack-learning-backend.vercel.app/api/auth",
     withCredentials: true
 });
